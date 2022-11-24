44855da805e097d7fc3a40424fb447eb5b594bb5
hafizjavaid <hafizjaidi786@gmail.com>
2022-11-24 12:36:36 +0500

    First Commit

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-switch class="d-flex justify-end mr-4" v-model="themeToggler"></v-switch>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="bg-grey-lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="n in 4" :key="n">
+            <v-col class="mt-2" cols="12">
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+const drawer = ref(null);
+const themeToggler = ref(false);
+watch(themeToggler, () => {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+});
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+
+export default defineNuxtConfig({
+    css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.min.css'],
+    build: {
+        transpile: ['vuetify']
+    }
+})
